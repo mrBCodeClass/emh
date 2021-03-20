3553692c9983c32a5f3a2d7ef77f5bba140648ce
mrBCodeClass <77644060+mrBCodeClass@users.noreply.github.com>
2021-03-19 22:37:14 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function dropdownRedirect(url) {								
+	window.open(url, '_blank');					
+}
+
+let emhLogo = 'https://static.wixstatic.com/media/23ea77_d826538b69a84e61b9d002506df335d8~mv2.jpg/v1/fill/w_540,h_410,al_c,lg_1,q_80/hornet-logo-blue-500px.webp';
+
+function mainPage () {
+  
+let emhProj = ['Jesus V', 'Freitty G', 'Melanie S', 'Chenay C'];
+let siteName = ['freitty', 'jesus', 'melanie', 'chenay'];
+let text = "<h3> Mr. B's Coding Class Projects </h3>";
+  text += '<p><img src="' + emhLogo + '"/></p>';
+text += '<h5> Hostos Bio Page </h5><select onChange="dropdownRedirect(this.value)"><option>Choose below</option>';
+
+  for (i = 0; i < siteName.length; i++) {
+  text += "<option>" + siteName[i] + ".html</option>";
+}
+text += '</select>';  
+  
+    document.getElementById('contentArea').innerHTML = text;
+ 
+  
+}
+
+mainPage();
+
