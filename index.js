1b7de2be8885ec344b0362c0e03d77b12a427618
mrBCodeClass <77644060+mrBCodeClass@users.noreply.github.com>
2021-03-19 22:39:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ let emhLogo = 'https://static.wixstatic.com/media/23ea77_d826538b69a84e61b9d0025
 
 function mainPage () {
   
-let emhProj = ['Jesus V', 'Freitty G', 'Melanie S', 'Chenay C'];
-let siteName = ['freitty', 'jesus', 'melanie', 'chenay'];
+let emhProj = ['Jesus V', 'Freitty G', 'Jocelyn', 'Chenay'];
+let siteName = ['freitty', 'jesus', 'jocelyn', 'chenay'];
 let text = "<h3> Mr. B's Coding Class Projects </h3>";
   text += '<p><img src="' + emhLogo + '"/></p>';
 text += '<h5> Hostos Bio Page </h5><select onChange="dropdownRedirect(this.value)"><option>Choose below</option>';
